8fccf004808bba4ee24accebafcf2d05b43765c9
vicopra <vicopra@gmail.com>
2026-09-16 20:00:04 +0200

    change i made on my file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello, World!");
-
-
+console.log("I am starting my journey in software development!");
+console.log("I’m happy to be part of the team.");
+console.log("Hello, World!");
